9e3d58bc665d08d499419600146c76b04498bcf0
a96t27 <71828484+a96t27@users.noreply.github.com>
2024-03-02 16:39:42 +0200

    Testing: enemy following pointer

--- enemy/testing/testing.gd
+++ enemy/testing/testing.gd
@@ -4,6 +4,9 @@ extends Node2D
 @onready var tile_map = $TileMap
 @onready var marker_2d = $Marker2D
 
+func _process(delta):
+	marker_2d.global_position = get_global_mouse_position()
+
 func _on_tile_map_child_entered_tree(node):
 	if node.is_in_group(&"enemy"):
 		node.player = marker_2d
