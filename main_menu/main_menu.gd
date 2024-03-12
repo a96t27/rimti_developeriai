9583b4595027126928a494e85d5180dedf393b4f
a96t27 <71828484+a96t27@users.noreply.github.com>
2024-03-12 23:14:34 +0200

    Fixed: opening option menu

--- main_menu/main_menu.gd
+++ main_menu/main_menu.gd
@@ -5,7 +5,7 @@ extends Control
 @onready var quit_button = %Quit_Button
 @export var start_level: PackedScene
 @onready var options_menu = %Options_Menu as OptionsMenu
-@onready var margin_container = $MarginContainer as MarginContainer
+@onready var v_box_container: VBoxContainer = $VBoxContainer
 
 func _ready():
 	handle_connecting_signals()
@@ -17,12 +17,12 @@ func on_exit_pressed() -> void:
 	get_tree().quit()
 
 func on_options_pressed() -> void:
-	margin_container.visible = false
+	v_box_container.visible = false
 	options_menu.set_process(true)
 	options_menu.visible = true
 
 func on_back_options_menu() -> void:
-	margin_container.visible = true
+	v_box_container.visible = true
 	options_menu.visible = false
 
 func handle_connecting_signals() -> void:
