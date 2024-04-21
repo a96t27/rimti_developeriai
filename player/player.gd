dc6983a1bf05924f627060f4e4426690ae851eb4
wyliuss <geciauskasvilius@gmail.com>
2024-04-21 21:18:06 +0300

    pataisytas ejimas

--- player/player.gd
+++ player/player.gd
@@ -19,26 +19,42 @@ func _physics_process(delta: float) -> void:
 		return
 		
 	get_input()
-	if movement_direction == Vector2.ZERO:
-		movement_direction = next_movement_direction
-	if can_move_in_direction(next_movement_direction, delta):
-		movement_direction = next_movement_direction
-	velocity = movement_direction * speed
-	move_and_slide()
+	try_change_direction()
+	move_character(delta)
 
 func get_input() -> void:
+	var dir = Vector2.ZERO
 	if Input.is_action_pressed("move_left"):
-		next_movement_direction = Vector2.LEFT
+		dir = Vector2.LEFT
 		rotation_degrees = 0
 	elif Input.is_action_pressed("move_right"):
-		next_movement_direction = Vector2.RIGHT
+		dir = Vector2.RIGHT
 		rotation_degrees = 180
 	elif Input.is_action_pressed("move_up"):
-		next_movement_direction = Vector2.UP
+		dir = Vector2.UP
 		rotation_degrees = 90
 	elif Input.is_action_pressed("move_down"):
-		next_movement_direction = Vector2.DOWN
+		dir = Vector2.DOWN
 		rotation_degrees = 270
+
+	if dir != Vector2.ZERO:
+		next_movement_direction = dir
+
+func try_change_direction() -> void:
+	if next_movement_direction != Vector2.ZERO and can_move_in_direction(next_movement_direction, 0):
+		# Change the direction if the new direction is valid
+		movement_direction = next_movement_direction
+		next_movement_direction = Vector2.ZERO
+	elif not can_move_in_direction(movement_direction, 0):
+		# If the current direction is blocked, stop the movement
+		movement_direction = Vector2.ZERO
+		
+func move_character(delta: float) -> void:
+	if movement_direction != Vector2.ZERO:
+		velocity = movement_direction * speed
+		move_and_slide()
+	else:
+		velocity = Vector2.ZERO
 
 func death() -> void:
 	get_tree().change_scene_to_file("res://game_over_menu/game_over_menu.tscn")
@@ -49,7 +65,7 @@ func take_damage() -> void:
 	emit_signal("health_changed", current_health)
 
 func can_move_in_direction(dir: Vector2, delta: float) -> bool:
-	shape_query.transform = global_transform.translated(dir * speed * delta * 2)
+	shape_query.transform = global_transform.translated(dir * (speed * delta + 1))
 	var result = get_world_2d().direct_space_state.intersect_shape(shape_query)
 	return result.size() == 0 
 
