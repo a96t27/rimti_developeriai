d68cdeb328c86ace21950f798d007cdd15438d0e
Lumin <lukas.bilinskas@ktu.edu>
2024-05-15 11:41:34 +0300

    Added winning screen

---
+++ level/winning_screen.gd
@@ -0,0 +1,15 @@
+class_name WinningScreen
+extends Control
+
+var main_menu: PackedScene = load("res://main_menu/main_menu.tscn")
+var start_level: PackedScene = load("res://level/level.tscn")
+func _on_restart_button_pressed():
+	get_tree().change_scene_to_packed(start_level)
+
+
+func _on_main_menu_button_pressed():
+	get_tree().change_scene_to_packed(main_menu)
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
